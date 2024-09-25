9f18220d032b4665b5882c3a5981af274a18d451
Miheer Pandey <me.miheer@gmail.com>
2024-09-25 16:35:50 +0530

    added

--- admin/uploads.php
+++ admin/uploads.php
@@ -3,6 +3,7 @@ require('../connection.php');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 header('Content-Type: text/html; charset=utf-8');
 
 
