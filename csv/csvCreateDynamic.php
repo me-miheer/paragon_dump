6e6a1bff4bb7778f587874a789c6ce93568e8c77
Miheer Pandey <me.miheer@gmail.com>
2025-09-23 15:19:08 +0530

    removed string extra text

--- csv/csvCreateDynamic.php
+++ csv/csvCreateDynamic.php
@@ -55,7 +55,7 @@ if(empty($_POST['name']) || empty($_POST['qr']) || empty($_POST['shop']) || empt
 
 
 // Setting up all the parameters 
-$name = mysqli_real_escape_string($mysql,trim(string: $_POST['name']));
+$name = mysqli_real_escape_string($mysql,trim($_POST['name']));
 $qr = mysqli_real_escape_string($mysql,trim($_POST['qr']));
 $dealer = mysqli_real_escape_string($mysql,trim($_POST['dealer']));
 $shop = mysqli_real_escape_string($mysql,trim($_POST['shop']));
