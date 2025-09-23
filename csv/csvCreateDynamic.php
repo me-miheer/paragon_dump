46b7e68d2cecb811af755736bf6c567ed272bd85
Miheer Pandey <me.miheer@gmail.com>
2025-09-23 15:22:26 +0530

    testing

--- csv/csvCreateDynamic.php
+++ csv/csvCreateDynamic.php
@@ -1,5 +1,9 @@
 <?php
 require('../connection.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 //check weather request method is post
 if($_SERVER['REQUEST_METHOD'] != 'POST'){
     http_response_code(403);
